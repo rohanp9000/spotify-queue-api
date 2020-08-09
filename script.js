354e137bccb722ade549894dae7710cfce451431
rohanp9000 <rohanp9000@gmail.com>
2020-08-09 18:10:54 -0400

    turned off headless

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
     await page.goto('https://sptfy-auth-server.herokuapp.com/login', {waitUntil: 'networkidle2'});
