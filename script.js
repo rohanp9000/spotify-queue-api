491bfaae7fe0f984035b7fd323fcf7c90a6faae5
rohanp9000 <rohanp9000@gmail.com>
2020-08-09 18:05:08 -0400

    added script

---
+++ script.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+
+async function run() {
+  const browser = await puppeteer.launch({headless: false});
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    await page.goto('https://sptfy-auth-server.herokuapp.com/login', {waitUntil: 'networkidle2'});
+
+
+    await page.waitFor('#login-username')
+    await page.type('#login-username', 'aetherpyro')
+   
+    await page.waitFor('#login-password')
+    await page.type('#login-password', 'rohanP2019?')
+
+    await page.waitFor('#login-button')
+    await page.click('#login-button')
+
+    await page.waitFor(3000);
+    
+     await browser.close();
+   }
+   run().then(() => {
+    console.log('Program ran at :' + new Date());
+  })
+
